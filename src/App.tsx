b09ed4b55ce5417bdc8b3d63089f58045341a912
Take-Yuki NAGAO <tnagao@TYN-MacBook-Pro.local>
2024-08-02 12:34:23 +0900

    modify style

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,6 @@ import { Component } from 'react';
 import YouTube from 'react-youtube';
 import './YouTubePlayer.css';
 export default class App extends Component {
-  style = {
-    position: "absolute",
-    left: 0,
-    top: 0,
-    width: "100%",
-    height: "100%",
-  }
   opts = {
     playerVars: {
       autoplay: 1,
@@ -24,8 +17,7 @@ export default class App extends Component {
       loop: 0,
       volume: 0,
       iv_load_policy: 3
-    },
-    ...this.style
+    }
   }
 
   onReady(event : any) {
@@ -39,8 +31,9 @@ export default class App extends Component {
 
   render() {
   return (
-    <>
-      <YouTube videoId='YEWEVXEt0ko'
+    <div className='youtube-container'>
+      <YouTube className='youtube-video'
+               videoId='YEWEVXEt0ko'
                opts={this.opts}
                onReady={this.onReady}
                onEnd={this.onEnd}
